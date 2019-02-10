b5d9c71b218c9b7b9a8dd8cbb3e1b3212b9a4ee6
rbangamm <ravindu.angammana@gmail.com>
2019-02-10 15:56:59 -0500

    Added query parameter filtering for min_frequency and created a Jinja2 template to display the skills as an HTML table.

--- backend/api/skills.py
+++ backend/api/skills.py
@@ -1,5 +1,6 @@
 import backend
-from flask import Blueprint, jsonify, request
+from prettytable import PrettyTable
+from flask import Blueprint, jsonify, request, render_template
 
 skills = Blueprint('skills', __name__)
 
@@ -15,6 +16,9 @@ def get_skills():
     if min_freq is None:
         min_freq = 0
 
+    min_rating = int(min_rating)
+    min_freq = int(min_freq)
+
     skills = backend.query_db('select * from skills')
     skill_count = {}
     skill_rating = {}
@@ -27,13 +31,20 @@ def get_skills():
                 skill_count[skill['name']] += 1
                 skill_rating[skill['name']] += skill['rating']
     
-    for skill, count in skill_count.iteritems():
+    skills_to_pop = []
+    for skill, count in skill_count.items():
         if count < min_freq:
-            del skill_count[skill]
-            del skill_rating[skill]
+            skills_to_pop.append(skill)
 
+    for skill in skills_to_pop:
+        del skill_count[skill]
+        del skill_rating[skill]
+
+    items = []
     for name, rating in skill_rating.items():
         skill_rating[name] = rating / skill_count[name]
         skill_rating[name] = round(skill_rating[name], 1)
+        items.append({'name' : name, 'count' : skill_count[name], 
+                  'rating' : skill_rating[name]})
 
-    return '%s' % skill_rating
+    return render_template('skills.html', items=items)
