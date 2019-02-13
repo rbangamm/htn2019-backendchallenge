3e3b29f06a94e998e56ede817c346070e95edd79
rbangamm <ravindu.angammana@gmail.com>
2019-02-12 20:38:39 -0500

    Allowed POST requests to All Users endpoint

--- backend/api/users.py
+++ backend/api/users.py
@@ -1,6 +1,7 @@
 import backend
 import backend.helpers
-from flask import Blueprint, jsonify, request
+import re
+from flask import Blueprint, jsonify, request, abort
 
 users = Blueprint('users', __name__)
 
@@ -52,3 +53,80 @@ def update_user(user_id):
     user_skills = backend.query_db('select * from skills where user_id = ?', [user_id])
     backend.helpers.format_user(user_skills, user)
     return jsonify(user)
+
+@users.route('/', methods=['POST'])
+def create_user():
+    c = backend.get_db().cursor()
+    data = request.json
+    schema = backend.get_schema('users')
+    
+    # Since skills is not in table schema
+    schema.append('skills')
+
+    schema.remove('id')
+
+    keys = set(data.keys())
+    schema_set = set(schema)
+
+    # If the data is missing keys, abort
+    if schema_set.difference(keys) != set():
+        abort(400)
+
+    ### Data sanity checks ###
+    phone_exp = re.compile("^(?:\+?1[-. ]?)?\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$")
+    email_exp = re.compile("^[\w\-\.]+@([\w-]+\.)+[\w-]{2,4}$")
+    
+    # Check if name, picture and company are strings
+    if (not isinstance(data["name"], str) or 
+        not isinstance(data["picture"], str) or
+        not isinstance(data["company"], str)):
+        abort(400)
+
+    # Check if phone number has a valid format
+    if not phone_exp.match(data["phone"]):
+        abort(400)
+
+    # Check if email address has a valid format
+    if not email_exp.match(data["email"]):
+        abort(400)
+
+    # Check if latitude and longitude are floats
+    try:
+        latitude = float(data["latitude"])
+        longitude = float(data["longitude"])
+    except ValueError as err:
+        abort(400)
+
+    # Check if skills is a list
+    if not isinstance(data["skills"], list):
+        abort(400)
+
+    tup = (data["name"], data["picture"], data["company"], data["email"], 
+           data["phone"], data["latitude"], data["longitude"])
+    c.execute('''INSERT INTO users 
+                (name, picture, company, email, phone, latitude, longitude)
+                VALUES (?, ?, ?, ?, ?, ?, ?)''', tup)
+
+    # The last row id will be the user_id for the skill
+    last_row = c.lastrowid
+    
+    skills_to_append = []
+    for skill in data["skills"]:
+        if (not isinstance(skill.get("name"), str) or 
+            not isinstance(skill.get("rating"), int)):
+            abort(400)
+        skills_to_append.append((skill["name"], skill["rating"], last_row))
+    
+    c.executemany('''INSERT INTO skills (name, rating, user_id)
+                     VALUES (?, ?, ?) ''', skills_to_append)
+    
+    backend.get_db().commit()
+    user = backend.query_db('SELECT * FROM users WHERE id = ?', [last_row], one=True)
+    user_skills = backend.query_db('SELECT * FROM skills WHERE user_id = ?', [last_row])
+    backend.helpers.format_user(user_skills, user)
+    return jsonify(user)
+
+
+@users.errorhandler(400)
+def user_error(e):
+    return "400 - Invalid data", 400
